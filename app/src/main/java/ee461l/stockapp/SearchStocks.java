aea5d57ac0dfde42ac6f0ca289dca572ceddffa4
Mitchell <robert.whitlatch@utexas.edu>
2018-11-13 15:05:48 -0600

    Fixed placement of search bar and a slight bug

--- app/src/main/java/ee461l/stockapp/SearchStocks.java
+++ app/src/main/java/ee461l/stockapp/SearchStocks.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SearchView;
 
 public class SearchStocks extends AppCompatActivity {
 
