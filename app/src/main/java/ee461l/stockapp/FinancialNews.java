22c2edad0f348e8d3014eef0cdd507b6b9f3751d
Mitchell <robert.whitlatch@utexas.edu>
2018-11-12 22:19:32 -0600

    Chose better starting templates for the different pages. Cleaned up old page materials.

--- app/src/main/java/ee461l/stockapp/FinancialNews.java
+++ app/src/main/java/ee461l/stockapp/FinancialNews.java
@@ -1,7 +1,11 @@
 package ee461l.stockapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class FinancialNews extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class FinancialNews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financial_news);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
--- app/src/main/java/ee461l/stockapp/SearchStocks.java
+++ app/src/main/java/ee461l/stockapp/SearchStocks.java
@@ -1,7 +1,11 @@
 package ee461l.stockapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class SearchStocks extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class SearchStocks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_stocks);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
