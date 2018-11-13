9ab6e43fc7136eb3960d506a08b8b725e773cb90
Mitchell <robert.whitlatch@utexas.edu>
2018-11-13 14:55:20 -0600

    Added search bar to Search Stocks.

--- app/src/main/java/ee461l/stockapp/SearchStocks.java
+++ app/src/main/java/ee461l/stockapp/SearchStocks.java
@@ -4,17 +4,30 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class SearchStocks extends AppCompatActivity {
 
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_stocks);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        searchView = (SearchView) findViewById(R.id.search_field);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Index against data source here
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
